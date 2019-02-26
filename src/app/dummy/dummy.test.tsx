742af94bdb82784b666b8164feaf05ac5f5aa429
Rafael Yates <r.yates@live.com>
2019-02-25 21:12:53 -0300

    Include redux mock to dummy test
    Improves the dummy test to include redux,
    but it is still returning an empty object.

--- src/app/dummy/dummy.test.tsx
+++ src/app/dummy/dummy.test.tsx
@@ -1,11 +1,18 @@
-import { shallow, ShallowWrapper } from 'enzyme';
+import configureStore, { MockStoreCreator, MockStoreEnhanced } from 'redux-mock-store';
+import promise from 'redux-promise';
+import thunk from 'redux-thunk';
+
+import { ShallowWrapper } from 'enzyme';
+import { shallowWithStore } from 'enzyme-redux';
 
 import { DummyComponent } from '@app/dummy/dummy.component';
 
 describe('Component: Dummy', () => {
+  const mockStoreCreator: MockStoreCreator = configureStore([thunk, promise]);
+  const store: MockStoreEnhanced = mockStoreCreator({});
 
   it('should render without crashing', () => {
-    const wrapper: ShallowWrapper = shallow(<DummyComponent />);
+    const wrapper: ShallowWrapper = shallowWithStore(<DummyComponent />, store);
     expect(wrapper).toBeDefined();
   });
 
