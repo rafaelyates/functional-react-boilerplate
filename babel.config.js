550326b7847f0994cdcccf6d8256ad9098392875
Rafael Yates <r.yates@live.com>
2019-03-03 17:15:26 -0300

    Improve babel configuration
    Removes the typescript babel preset, as this task
    is already being handled by typescript loaders.
    
    Includes new loaders and presets for default builds
    and includes specific configuration for production
    builds.

--- babel.config.js
+++ babel.config.js
@@ -5,18 +5,34 @@ module.exports = (api) => {
 
   return ({
     presets: [
-      "@babel/typescript",
-      "@babel/preset-env",
-      "@babel/preset-react",
-      "airbnb"
+      '@babel/preset-env',
+      '@babel/preset-react',
+      'backpack',
+      'airbnb'
     ],
     plugins: [
-      "@babel/plugin-syntax-dynamic-import",
-      "@babel/plugin-syntax-import-meta",
-      "@babel/plugin-syntax-export-namespace-from",
-      "@babel/plugin-syntax-throw-expressions"
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-syntax-import-meta',
+      '@babel/plugin-syntax-export-namespace-from',
+      '@babel/plugin-syntax-throw-expressions',
+      '@babel/plugin-syntax-object-rest-spread',
+      '@babel/plugin-syntax-json-strings',
+      'dynamic-import-node',
+      'lodash'
     ],
     sourceMaps: true,
-    compact: true
+    env: {
+      production: {
+        presets: [
+          'minify',
+        ],
+        plugins: [
+          '@babel/plugin-transform-react-inline-elements',
+          '@babel/plugin-transform-react-constant-elements',
+          'transform-react-remove-prop-types'
+        ],
+        compact: true
+      }
+    }
   });
 }
