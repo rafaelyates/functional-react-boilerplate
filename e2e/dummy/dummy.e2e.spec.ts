b4962e3646b83aef9353dd45ebd01fdd3d21b0cb
Rafael Yates <r.yates@live.com>
2019-03-26 22:55:20 -0300

    Fix e2e test
    Fixes the e2e test scenario.

--- e2e/dummy/dummy.e2e.spec.ts
+++ e2e/dummy/dummy.e2e.spec.ts
@@ -5,12 +5,16 @@ describe('end to end: Dummy', () => {
 
   it('Should see the text typed prefixed by hello in the title', async () => {
     const testName: string = name.firstName();
+    const capitalizedName: string = testName.replace(
+      /\w*\S/gi,
+      (value: string) => `${value.charAt(0).toUpperCase()}${value.slice(1).toLowerCase()}`,
+    );
 
     return page
       .waitForSelector('.title')
       .then(async () => page.click('input[name=userName]'))
       .then(async () => page.type('input[name=userName]', testName))
       .then(async () => page.$eval('.title', (element: Element) => element.innerHTML))
-      .then((text: string) => expect(text).toBe(`Hello ${testName}!!`));
+      .then((text: string) => expect(text).toBe(`Hello ${capitalizedName}!!`));
   });
 });
